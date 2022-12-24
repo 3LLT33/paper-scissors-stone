c9c3788d7c54045c03cf94ff5168a8fddb4c0363
3LLT33 <LT.github@proton.me>
2022-12-24 17:01:23 +0800

    First commit of the Odin Project RPS project

---
+++ paperscissorstone.js
@@ -0,0 +1,20 @@
+//Computer randomly chooses
+getComputerChoice
+computerSelection
+
+//User enters a choice
+//Use three buttons
+userSelection
+
+//Play the game
+function playTheGame() {
+}
+
+//Output user win or loss
+window.alert
+"You Lose! Paper beats Rock!"
+"You Lose! Rock beats Scissors!"
+"You Lose! Scissors beat Paper!"
+"You Win! Paper beats Rock!"
+"You Win! Rock beats Scissors!"
+"You Win! Scissors beat Paper!"
